93bb117b79cff5b51c60521aab7a486739561b1c
Erik Dubois <erik.dubois@gmail.com>
2018-02-25 12:50:03 +0100

    archmerge out

--- calamares-3-1-4/build-v11.sh
+++ calamares-3-1-4/build-v11.sh
@@ -23,8 +23,8 @@ makepkg --sign
 #### cleaning up
 
 echo "moving created files to repo"
-mv $search*pkg.tar.xz ../../archmerge_repo_iso/x86_64/
-mv $search*pkg.tar.xz.sig ../../archmerge_repo_iso/x86_64/
+mv $search*pkg.tar.xz ../../arcolinux_repo_iso/x86_64/
+mv $search*pkg.tar.xz.sig ../../arcolinux_repo_iso/x86_64/
 
 
 
